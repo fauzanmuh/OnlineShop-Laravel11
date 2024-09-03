5bfeb533bcca83d33c7b2ec3e8d20d84aa3b5d52
fauzanmuh <f4uz4n.oioi@gmail.com>
2024-09-03 21:17:58 +0700

    make a authentication  view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Livewire\Auth\ForgotPasswordPage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\CancelPage;
 use App\Livewire\CartPage;
 use App\Livewire\CheckoutPage;
 use App\Livewire\DetailProdukPage;
@@ -8,6 +13,8 @@ use App\Livewire\KategoriPage;
 use App\Livewire\MyOrderDetailPage;
 use App\Livewire\MyOrderPage;
 use App\Livewire\ProdukPage;
+use App\Livewire\SuksesPage;
+use Filament\Pages\Auth\Register;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -22,3 +29,15 @@ Route::get('/produk/{produk}', DetailProdukPage::class)->name('produk.show');
 Route::get('/checkout', CheckoutPage::class);
 Route::get('/my-order', MyOrderPage::class);
 Route::get('/my-order/{order}', MyOrderDetailPage::class);
+
+/**
+ * Authentication Routes
+ */
+
+ Route::get('/login', LoginPage::class);
+ Route::get('/register', RegisterPage::class);
+ Route::get('/forgot-password',ForgotPasswordPage::class);
+ Route::get('/reset-password', ResetPasswordPage::class);
+
+ Route::get('/sukses', SuksesPage::class);
+ Route::get('/cancel', CancelPage::class);
