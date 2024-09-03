406cd7505f2ffa30b14f3af3f026b6450acc8cf0
fauzanmuh <f4uz4n.oioi@gmail.com>
2024-09-03 22:28:11 +0700

    connect api for homepage

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Kategori;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Home Page - Tokopaido')]
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        $brand = Brand::where('is_active', true)->get();
+        $kategori = Kategori::where('is_active', true)->get();
+        return view('livewire.home-page', [
+            'brand' => $brand,
+            'kategori' => $kategori
+        ]);
     }
 }
