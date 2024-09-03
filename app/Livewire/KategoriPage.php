12a3dfa1fd6db161d6286b8a7349036c9aafdf4b
fauzanmuh <f4uz4n.oioi@gmail.com>
2024-09-03 22:40:00 +0700

    implement api form db for kategori

--- app/Livewire/KategoriPage.php
+++ app/Livewire/KategoriPage.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Kategori;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Kategori - Tokopaido')]
 class KategoriPage extends Component
 {
     public function render()
     {
-        return view('livewire.kategori-page');
+        $kategori = Kategori::where('is_active', true)->get();
+        return view('livewire.kategori-page', [
+            'kategori' => $kategori
+        ]);
     }
 }
